da614c158cac8817e0e5c5f8f2f7c45609241b54
Peter Fortune <peterekene162@gmail.com>
2026-01-27 00:26:06 +0100

    latest changes

--- herome_fab/settings.py
+++ herome_fab/settings.py
@@ -35,7 +35,7 @@ DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = env.list(
     "ALLOWED_HOSTS",
-    default=["127.0.0.1", "localhost", "herome-fab-api.herokuapp.com"]
+    default=["127.0.0.1", "localhost", "herome-fab-api-985031da9a39.herokuapp.com"]
 )
 
 
