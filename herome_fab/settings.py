fb3879e99c9a660df18cd3ddbc9a636c2a48a052
Peter Fortune <peterekene162@gmail.com>
2026-01-26 23:34:29 +0100

    fixed allowed host for local and heroku

--- herome_fab/settings.py
+++ herome_fab/settings.py
@@ -35,7 +35,7 @@ DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = env.list(
     "ALLOWED_HOSTS",
-    default=["127.0.0.1", "localhost", ".herokuapp.com"]
+    default=["127.0.0.1", "localhost", "herome-fab-api.herokuapp.com"]
 )
 
 
